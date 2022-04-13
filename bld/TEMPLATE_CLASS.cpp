7f728c96a34ec3e6f25044227e3ac2bc08665f1a
Smartskaft2 <smartskaft2@gmail.com>
2022-04-14 00:42:50 +0200

    feat/refactor: Windows keyboard handler, key events, TID time display
    I am too tired to split this right now...

---
+++ bld/TEMPLATE_CLASS.cpp
@@ -0,0 +1,12 @@
+#include "Utils/Log.h"
+#include "TEMPLATE_CLASS.h"
+
+#include <string>
+#include <iostream>
+
+namespace Ra180 {
+
+
+
+} // namespace Ra180
+
---
+++ bld/TEMPLATE_CLASS.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+namespace Ra180 {
+
+    class TEMPLATE_CLASS
+    {
+    public:
+    private:
+    };
+
+} // namespace Ra180
+
