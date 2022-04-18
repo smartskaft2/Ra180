d1c49236d3b32415c8e5a9cd75336205d7ebe328
Smartskaft2 <smartskaft2@gmail.com>
2022-04-18 15:26:24 +0200

    refactor: State KLAR uses ArrowUp/Down key events to transition

--- src/States/KLAR.cpp
+++ src/States/KLAR.cpp
@@ -10,8 +10,8 @@ namespace Ra180 {
 
     KLAR::KLAR(Radio180& radio) : Radio180State("KLAR", "klar", radio)
     {
-        AddTransition({ Event::Type::SKYDD, {}, {}, [&]{ return CreateState<SKYDD>(); } });
-        AddTransition({ Event::Type::FRÅN,   {}, {}, [&]{ return CreateState<FRÅN>();   } });
+        AddTransition({ Event::Type::KeyEvent, {KeyPressedGuard(KeyCode::ArrowUp)},      {}, [&]{ return CreateState<SKYDD>(); }});
+        AddTransition({ Event::Type::KeyEvent, {KeyPressedGuard(KeyCode::ArrowDown)},    {}, [&]{ return CreateState<FRÅN>();  }});
     }
 
 } // namespace Ra180
