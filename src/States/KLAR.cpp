ffc3e03c8dfd9d2bf8754fc08845466ba46d7f6a
Smartskaft2 <smartskaft2@gmail.com>
2022-04-18 17:58:02 +0200

    feat: Add state transitions from KLAR and SKYDD to TID.

--- src/States/KLAR.cpp
+++ src/States/KLAR.cpp
@@ -1,10 +1,11 @@
 
 #include "Events/Event.h"
 #include "Radio/Radio180.h"
-#include "States/KLAR.h"
-#include "States/SKYDD.h"
 #include "States/FRÅN.h"
+#include "States/KLAR.h"
 #include "States/ModeState.h"
+#include "States/SKYDD.h"
+#include "States/TID.h"
 
 namespace Ra180 {
 
@@ -12,6 +13,7 @@ namespace Ra180 {
     {
         AddTransition({ Event::Type::KeyEvent, {KeyPressedGuard(KeyCode::ArrowUp)},      {}, [&]{ return CreateState<SKYDD>(); }});
         AddTransition({ Event::Type::KeyEvent, {KeyPressedGuard(KeyCode::ArrowDown)},    {}, [&]{ return CreateState<FRÅN>();  }});
+        AddTransition({ Event::Type::KeyEvent, {KeyPressedGuard(ToKeyCode(HMIID::TID))}, {}, [&]{ return CreateState<TID>();   }});
     }
 
 } // namespace Ra180
--- src/States/SKYDD.cpp
+++ src/States/SKYDD.cpp
@@ -1,15 +1,17 @@
 
 #include "Events/Event.h"
 #include "Radio/Radio180.h"
-#include "States/SKYDD.h"
 #include "States/KLAR.h"
 #include "States/ModeState.h"
+#include "States/SKYDD.h"
+#include "States/TID.h"
 
 namespace Ra180 {
 
     SKYDD::SKYDD(Radio180& radio) : ModeState("SKYDD", "skydddddddd", radio, Radio180::Mode::SKYDD)
     {
         AddTransition({ Event::Type::KeyEvent, {KeyPressedGuard(KeyCode::ArrowDown)},    {}, [&]{ return CreateState<KLAR>(); }});
+        AddTransition({ Event::Type::KeyEvent, {KeyPressedGuard(ToKeyCode(HMIID::TID))}, {}, [&]{ return CreateState<TID>();  }});
     }
 
 } // namespace Ra180
